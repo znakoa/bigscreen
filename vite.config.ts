98473391f35e692eee9708e7d3c85ef859c76122
znakoa <zhougui@zjwykjyxgs13.wecom.work>
2024-08-15 14:07:20 +0800

    首次

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,22 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { viteMockServe} from 'vite-plugin-mock'
+import { viteMockServe } from 'vite-plugin-mock'
 
 function pathResolve(dir: string) {
   return resolve(process.cwd(), '.', dir)
 }
 
 export default defineConfig({
-  base:'/',
+  base: '.',
   resolve: {
     alias: [
       {
         find: '@',
-        replacement: pathResolve('src')
-      }
+        replacement: pathResolve('src'),
+      },
     ],
-    dedupe: ['vue']
+    dedupe: ['vue'],
   },
   plugins: [
     viteMockServe({
@@ -24,8 +24,8 @@ export default defineConfig({
       // 生产打包开关
       enable: true,
       // 监视文件更改
-      watchFiles: true
+      watchFiles: true,
     }),
     react(),
-  ]
+  ],
 })
